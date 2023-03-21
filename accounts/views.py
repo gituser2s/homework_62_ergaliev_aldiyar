c5a946826df0fabea08b24262c7203bcc201aee0
Nickname Lastname <adada2231wadsa324@gmail.com>
2023-03-21 18:03:27 +0600

    Файл который содержит в себе представления Логина и Регистрации на сайте

---
+++ accounts/views.py
@@ -0,0 +1,64 @@
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import redirect, reverse
+from django.views.generic import TemplateView, CreateView
+from accounts.forms import LoginForm, CustomUserCreationForm
+
+
+class LoginView(TemplateView):
+    template_name = 'login.html'
+    form = LoginForm
+
+    def get(self, request, *args, **kwargs):
+        form = self.form()
+        context = {'form': form}
+        return self.render_to_response(context)
+
+    def post(self, request, *args, **kwargs):
+        form = self.form(request.POST)
+        if not form.is_valid():
+            messages.error(request, "Некорректные данные!")
+            return redirect('index')
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password')
+        user = authenticate(request, username=username, password=password)
+        if not user:
+            messages.warning(request, "Пользователь в бд не найден!")
+            return redirect('index')
+        login(request, user)
+        messages.success(request, 'Добро пожаловать!')
+        next = request.GET.get('next')
+        if next:
+            return redirect(next)
+        return redirect('index')
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('index')
+
+
+class RegisterView(CreateView):
+    template_name = 'register.html'
+    form_class = CustomUserCreationForm
+    success_url = "/"
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect(self.success_url)
+        context = {'form': form}
+        return self.render_to_response(context)
+
+    def get_success_url(self):
+        next_url = self.request.GET.get('next')
+        if not next_url:
+            next_url = self.request.POST.get('next')
+        if not next_url:
+            next_url = reverse('index')
+        return next_url
+
+
+
